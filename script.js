cd2ef10ae5d2c3a9ad9c4c342756b9d783c63bd3
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-08-21 21:32:19 -0300

    Implementação requisito localStorage

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const query = 'computador';
 const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
 
-/* function getLocalStorage() {
-
-}
-
-function setLocalStorage() {
-
+/* function setLocalStorage(obj, param) {
+  const cart = JSON.parse(localStorage.getItem('cartItemList'));
+  if (param === '+') {
+    cart.push(obj);
+    localStorage.setItem('cartItemList', JSON.stringify(cart));
+  } else {
+    const deletedCartItem = cart.filter((element) => element.sku !== obj);
+    localStorage.setItem('cartItemList', JSON.stringify(deletedCartItem));   
+  }
 } */
 
 function totalPrice(param, valor) {
@@ -20,6 +23,8 @@ function totalPrice(param, valor) {
     total += valor;
     localStorage.setItem('total', ((total.toFixed(2))));
     spanPrice.innerHTML = `${total}`;
+  } else if (param === 'recover') {
+    spanPrice.innerHTML = `${valor}`;
   } else {
     total -= valor;
     localStorage.setItem('total', (total.toFixed(2)));
@@ -93,7 +98,11 @@ async function searchProducts(event) {
   });
 }
 
-async function cartItemClickListener(event) {
+function setLocalStorage(list) {
+  localStorage.setItem('cartItemList', list.innerHTML);
+}
+
+function cartItemClickListener(event) {
   // coloque seu código aqui
   const cartItemList = document.querySelector('.cart__items');
   if (event === 'clean') { // Apaga toda lista
@@ -101,8 +110,12 @@ async function cartItemClickListener(event) {
   } else { // Apaga somente um evento da lista
     const itemDeleted = event.target.innerText;
     const valor = parseFloat(itemDeleted.slice(itemDeleted.indexOf('$') + 1));
-    await totalPrice('-', valor);
+    // No item clicado, eu extraio o valor para remover do Valor Total
+    totalPrice('-', valor);
+    // const id = itemDeleted.slice(5, 18);
     cartItemList.removeChild(event.target);
+    setLocalStorage(cartItemList);
+    // será que consigo fazer um loop e encontrar em qual posição da lista foi clicado???
   }
 }
 
@@ -114,16 +127,56 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-async function insertItemCart(array) {
+function apendItemToCart(obj) {
+  const textTotalPrice = document.querySelector('.total-price-text');
+  textTotalPrice.classList.add('display');
+  const li = createCartItemElement(obj);
+  const listItems = document.querySelector('.cart__items');
+  listItems.appendChild(li);
+  setLocalStorage(listItems);
+}
+
+function insertItemCart(array) {
   const objToAdd = {
     sku: array.id,
     name: array.title,
     salePrice: array.price,
   };
-  await totalPrice('+', array.price);
-  const li = createCartItemElement(objToAdd);
-  const listItems = document.querySelector('.cart__items');
-  listItems.appendChild(li);
+  totalPrice('+', array.price);
+  apendItemToCart(objToAdd);
+  // setLocalStorage(objToAdd, '+');
+}
+
+/* function getLocalStorage() {
+  const loadCart = JSON.parse(localStorage.getItem('cartItemList'));
+  const recoverTotal = loadCart.reduce((acc, value) => acc + value.salePrice, 0);
+  const spanPrice = document.querySelector('.total-price');
+  spanPrice.innerHTML = recoverTotal;
+  if (loadCart.length > 0) {
+    for (let i = 0; i < loadCart.length; i += 1) {
+      apendItemToCart(loadCart[i]);
+    }
+  }
+} */
+
+function getLocalStorage() {
+  const localStorageData = localStorage.getItem('cartItemList'); // recupera lista do LocalStorage e armazena na variável localStorageDate
+  const arrayList = localStorageData.split('</li>'); // com a string gerada, cria um array de cada <li>
+  arrayList.pop(); // Remove último item do array que será sempre vazio
+  arrayList.forEach((item, index) => {
+    arrayList[index] = item.replace('<li class="cart__item">', ''); // remove a tag <li class="cart__item"> do inicio de cada item do array
+    const alias = arrayList[index]; // criei um apelido para encurtar o caminho
+    const obj = { // Crio um objeto de acordo com o que é recebido pela função 'apendItemToCart'
+      sku: alias.slice(5, alias.indexOf(' |')),
+      name: alias.slice(alias.indexOf('NAME: ') + 6, alias.indexOf(' | P')),
+      salePrice: alias.slice(alias.indexOf('$') + 1),
+    };
+    apendItemToCart(obj);
+  });
+  const localStorageTotalPrice = localStorage.getItem('total'); // Recupero o valor para mostrar no carrinho
+  if (localStorageTotalPrice !== null) { 
+    totalPrice('recover', localStorageTotalPrice); 
+  }
 }
 
 async function addCartItemElement(id) {
@@ -135,20 +188,22 @@ async function addCartItemElement(id) {
 }
 
 function initialRenderization() {
-/*   if (localStorage.getItem('cartItemList') === null) {
-    localStorage.setItem('cartItemList', ([]));
+  if (localStorage.getItem('cartItemList') === null) {
+    localStorage.setItem('cartItemList', JSON.stringify([]));
   } else {
     getLocalStorage();
-  } */
+  }
 }
 
-async function emptyCart() {
+function emptyCart() {
   const btnEmptyCart = document.querySelector('.empty-cart');
   btnEmptyCart.addEventListener('click', () => {
-  localStorage.clear();
-  const spanPrice = document.querySelector('.total-price');
-  spanPrice.innerHTML = '';
-  cartItemClickListener('clean'); // Limpar todos os campos da lista
+    localStorage.clear();
+    const spanPrice = document.querySelector('.total-price');
+    spanPrice.innerHTML = '';
+    cartItemClickListener('clean'); // Limpar todos os campos da lista
+    const textTotalPrice = document.querySelector('.total-price-text');
+    textTotalPrice.classList.remove('display');
     initialRenderization();
   });
 }
